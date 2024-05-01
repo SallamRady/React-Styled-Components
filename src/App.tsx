a83d6666ca1b0f75735d08f1423b85d58b7dcaad
Sallam Rady <sallamrady@gmail.com>
2024-05-01 16:18:43 +0300

    globel style in styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import "./App.css";
 import { StyledButton } from "./components/01.StyledButton";
 import { FancyButton } from "./components/02.FancyButton";
-import { AnimatedLogo } from './components/03.AnimatedImage';
+import { AnimatedLogo } from "./components/03.AnimatedImage";
+import { GlobalStyle } from "./components/04.GlobalStyles";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       {/* <img src={logo} alt="logo" className="App-logo" /> */}
       <AnimatedLogo src={logo} alt="logo" className="App-logo" />
       <div style={{ margin: "1rem" }}>
---
+++ src/components/04.GlobalStyles.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+button{
+    box-shadow: 2px 2px 2px 3px lightgray;
+}
+
+a{
+    text-decoration: none;
+}
+`;
