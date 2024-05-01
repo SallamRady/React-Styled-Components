2a8fadebb4a29a328622a1da0a937ead29c420ed
Sallam Rady <sallamrady@gmail.com>
2024-05-01 13:03:34 +0300

    basic syntax

---
+++ src/components/StyledButton.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  background-color: #bf4f74;
+  color: white;
+  font-size: 1em;
+  font-weight: 600;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #bf4f74;
+  border-radius: 3px;
+  cursor: pointer;
+`;
